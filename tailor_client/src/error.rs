04e1e8d2d96e7b26c2467a60c32d0805e30f94a4
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-12-02 22:37:23 +0100

    Tailor client: Improve error messages
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- tailor_client/src/error.rs
+++ tailor_client/src/error.rs
@@ -3,8 +3,8 @@ use zbus::fdo;
 
 #[derive(Error, Debug)]
 pub enum ClientError {
-    #[error("Bus response error")]
+    #[error("Bus response error: `{0}`")]
     BusError(#[from] fdo::Error),
-    #[error("Serialization error")]
+    #[error("Serialization error: `{0}`")]
     Serialization(#[from] serde_json::Error),
 }
