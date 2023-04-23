79f82d483849f46c06d86586c0c79110b2d17797
Marc Jakobi <marc.jakobi@tiko.energy>
2023-04-23 22:01:40 +0200

    copy default configs to /etc instead of symlink

--- nix/module.nix
+++ nix/module.nix
@@ -22,28 +22,40 @@ in {
   config = mkIf cfg.enable {
     hardware.tuxedo-keyboard.enable = true;
 
-    systemd.services.tailord = {
-      enable = lib.mkDefault true;
-      description = "Tux Tailor hardware control service";
-      after = ["systemd-logind.service"];
-      wantedBy = ["multi-user.target"];
+    systemd = {
+      services.tailord = {
+        enable = lib.mkDefault true;
+        description = "Tux Tailor hardware control service";
+        after = ["systemd-logind.service"];
+        wantedBy = ["multi-user.target"];
 
-      serviceConfig = {
-        Type = "dbus";
-        BusName = "com.tux.Tailor";
-        ExecStart = "${pkgs.tailord}/bin/tailord";
-        Environment = "RUST_BACKTRACE=1";
-        Restart = "on-failure";
+        serviceConfig = {
+          Type = "dbus";
+          BusName = "com.tux.Tailor";
+          ExecStart = "${pkgs.tailord}/bin/tailord";
+          Environment = "RUST_BACKTRACE=1";
+          Restart = "on-failure";
+        };
       };
     };
 
     services.dbus.packages = [pkgs.tailord];
 
+    # NOTE: By setting mode, the files are copied and not symlinked
     environment = {
       etc = {
-        "tailord/keyboard".source = ../tailord/default_configs/keyboard;
-        "tailord/fan".source = ../tailord/default_configs/fan;
-        "tailord/profiles".source = ../tailord/default_configs/profiles;
+        "tailord/keyboard/default.json" = {
+          source = ../tailord/default_configs/keyboard/default.json;
+          mode = "644";
+        };
+        "tailord/fan/default.json" = {
+          source = ../tailord/default_configs/fan/default.json;
+          mode = "644";
+        };
+        "tailord/profiles/default.json" = {
+          source = ../tailord/default_configs/profiles/default.json;
+          mode = "644";
+        };
       };
 
       systemPackages = mkIf cfg.tailor_gui.enable [
