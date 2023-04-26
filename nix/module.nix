325b22337b5dc445fb0019a9dbac3b1ba3a03b9f
Marc Jakobi <marc.jakobi@tiko.energy>
2023-04-27 00:34:37 +0200

    fix NixOS module

--- nix/module.nix
+++ nix/module.nix
@@ -23,52 +23,52 @@ in {
     };
   };
 
-  config = mkIf cfg.enable {
-    hardware.tuxedo-keyboard.enable = true;
+  config = mkIf cfg.enable (mkMerge [
+    {
+      hardware.tuxedo-keyboard.enable = true;
 
-    systemd = {
-      services.tailord = {
-        enable = lib.mkDefault true;
-        description = "Tux Tailor hardware control service";
-        after = ["systemd-logind.service"];
-        wantedBy = ["multi-user.target"];
+      systemd = {
+        services.tailord = {
+          enable = lib.mkDefault true;
+          description = "Tux Tailor hardware control service";
+          after = ["systemd-logind.service"];
+          wantedBy = ["multi-user.target"];
 
-        serviceConfig = {
-          Type = "dbus";
-          BusName = "com.tux.Tailor";
-          ExecStart = "${pkgs.tailord}/bin/tailord";
-          Environment = "RUST_BACKTRACE=1";
-          Restart = "on-failure";
-        };
-      };
-    };
-
-    services.dbus.packages = [pkgs.tailord];
-
-    # NOTE: By setting mode, the files are copied and not symlinked
-    environment = {
-      etc = {
-        "tailord/keyboard/default.json" = {
-          source = ../tailord/default_configs/keyboard/default.json;
-          mode = "644";
-        };
-        "tailord/fan/default.json" = {
-          source = ../tailord/default_configs/fan/default.json;
-          mode = "644";
-        };
-        "tailord/profiles/default.json" = {
-          source = ../tailord/default_configs/profiles/default.json;
-          mode = "644";
+          serviceConfig = {
+            Type = "dbus";
+            BusName = "com.tux.Tailor";
+            ExecStart = "${pkgs.tailord}/bin/tailord";
+            Environment = "RUST_BACKTRACE=1";
+            Restart = "on-failure";
+          };
         };
       };
 
-      systemPackages =
-        (mkIf cfg.tailor_gui.enable [
-          pkgs.tailor_gui
-        ])
-        ++ (mkIf cfg.tailor_cli.enable [
-          pkgs.tailor_cli
-        ]);
-    };
-  };
+      services.dbus.packages = [pkgs.tailord];
+
+      # NOTE: By setting mode, the files are copied and not symlinked
+      environment = {
+        etc = {
+          "tailord/keyboard/default.json" = {
+            source = ../tailord/default_configs/keyboard/default.json;
+            mode = "644";
+          };
+          "tailord/fan/default.json" = {
+            source = ../tailord/default_configs/fan/default.json;
+            mode = "644";
+          };
+          "tailord/profiles/default.json" = {
+            source = ../tailord/default_configs/profiles/default.json;
+            mode = "644";
+          };
+        };
+      };
+    }
+    {
+      environment.systemPackages = mkIf cfg.tailor_gui.enable [pkgs.tailor_gui];
+    }
+    {
+      environment.systemPackages = mkIf cfg.tailor_cli.enable [pkgs.tailor_cli];
+    }
+  ]);
 }
